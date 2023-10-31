5c884fc542cc04b93fc9fb5ea7a4e0b23ab1f286
ultramarinys <149311814+ultramarinys@users.noreply.github.com>
2023-10-31 18:12:59 +0300

    Update task_3.py

--- Лабораторная работа 1/task_3.py
+++ Лабораторная работа 1/task_3.py
@@ -1,11 +1,8 @@
 list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
 
-list1 = list_players[0:3] # участники первой команды
-list2 = list_players[3:6] # участники второй команды
+players_in_team = len(list_players)//2
+list1 = list_players[0:players_in_team] # участники первой команды
+list2 = list_players[players_in_team:] # участники второй команды
 
 print(list1)
 print(list2)
-
-
-
-
