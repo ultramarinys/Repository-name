804e3e4cfddc797c1c05f4e73257bc76e607542b
ultramarinys <149311814+ultramarinys@users.noreply.github.com>
2023-10-31 18:12:11 +0300

    Update task_2.py

--- Лабораторная работа 1/task_2.py
+++ Лабораторная работа 1/task_2.py
@@ -1,7 +1,9 @@
-# TODO Найдите количество книг, которое можно разместить на дискете
-symbols = 25*50*100 #количество символов
-count_b = 1.44*1024*1024 #размер в байтах
-count_sum = count_b/4
+pages = 100
+lines_in_page = 50
+symbols_in_line = 25
+symbols = pages*lines_in_page*symbols_in_line  # количество символов
+count_bytes = 1.44*1024*1024 # размер в байтах
+bytes_in_symbols = 4 # количество байт в одном символе
+count_sum = count_bytes/bytes_in_symbols
 count = int(count_sum/symbols)
 print("Количество книг, помещающихся на дискету:", count)
-
