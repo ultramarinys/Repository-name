b4b0ae705a6a31e8e316ef86207d3e19d9700441
ultramarinys <149311814+ultramarinys@users.noreply.github.com>
2023-11-27 21:32:00 +0300

    task 1.py

--- task 1.py
+++ task 1.py
@@ -6,7 +6,7 @@ INPUT_FILE = 'input.json'
 def task() -> float:
     with open(INPUT_FILE, 'r', encoding='utf-8') as f:
         item = json.load(f)
-    list_ = [float(i['score'])*float(i['weight']) for i in item]
+    list_ = [i['score']*i['weight'] for i in item]
     return round(sum(list_), 3)
 
 
